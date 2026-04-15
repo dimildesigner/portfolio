d1a541a6bca9dabb68d0333511a095fae6d27fd6
dimildesigner <dimildesignerdev@gmail.com>
2026-04-15 18:06:05 -0300

    Adicionado o arquivo vercel.json na raiz do projeto, para carregar o css no deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🔥 ESSA LINHA RESOLVE
+  // base: '/' é o padrão, não precisa nem declarar
 })
