03a9e647ceba56e07131df11d617bee43e82f4de
dimildesigner <dimildesignerdev@gmail.com>
2026-04-15 18:40:42 -0300

    Refazendo de outra forma

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Isso garante que os caminhos sejam relativos ao index.html
+  // base: '/' é o padrão, não precisa nem declarar
 })
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   // base: '/' é o padrão, não precisa nem declarar
-// })
