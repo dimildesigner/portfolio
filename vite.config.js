4ae9ad78b9721e477222cec74affc557f7d7bd33
dimildesigner <dimildesignerdev@gmail.com>
2026-04-15 18:28:14 -0300

    Adicionado no arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/' é o padrão, não precisa nem declarar
+  base: './styles/', // Isso garante que os caminhos sejam relativos ao index.html
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   // base: '/' é o padrão, não precisa nem declarar
+// })
