9a35657291e56d868ddfc7424cc731b900e8333c
dimildesigner <dimildesignerdev@gmail.com>
2026-04-15 18:31:07 -0300

    Novo ajuste no arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './styles/', // Isso garante que os caminhos sejam relativos ao index.html
+  base: './', // Isso garante que os caminhos sejam relativos ao index.html
 })
 
 
